6bf8668b5d3c427535efb82c68e7353ed0b1453e
Pranshu021 <kingpranshuduneja@gmail.com>
2019-02-23 03:01:29 +0530

    Some changes made

--- Oryx/views.py
+++ Oryx/views.py
@@ -55,7 +55,7 @@ def TestView(request):
     return render(request, 'test.html')
 
 def UploadView(request):
-    for i in os.listdir('E:\\Docs\\Oryx\\downloads'):
+    for i in os.listdir('..\\downloads'):
         obj = Smartphone()
         obj.name = re.sub('.jpeg' or '.jpg', '', i).lower()
         obj.image = i
