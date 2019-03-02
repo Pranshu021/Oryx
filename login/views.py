37d01769d70c8955b40f94ad05629393a92bec9e
Pranshu021 <kingpranshuduneja@gmail.com>
2019-03-03 04:20:34 +0530

    Removed some errors

--- login/views.py
+++ login/views.py
@@ -37,22 +37,27 @@ def LoginView(request):
                     return HttpResponse("Your account is disabled !")
             
             else:
-                if User.objects.get(email=username):
+                if User.objects.filter(email=username):
                     user = User.objects.get(email=username)
-                    login(request, user)
-                    request.session['user_id'] = user.id
-                    request.session.set_expiry(0)
-                    pk=user.id
-                    userinfo_object = UserInfo()
-                    if not UserInfo.objects.filter(Userinfo = request.user).exists():
-                        userinfo_object.Userinfo = request.user
-                        userinfo_object.save()
-                        
-                    return HttpResponseRedirect(reverse('home:home',))
-                
+                    if user.check_password(password):
+                        login(request, user)
+                        request.session['user_id'] = user.id
+                        request.session.set_expiry(0)
+                        pk=user.id
+                        userinfo_object = UserInfo()
+                        if not UserInfo.objects.filter(Userinfo = request.user).exists():
+                            userinfo_object.Userinfo = request.user
+                            userinfo_object.save()
+
+                        return HttpResponseRedirect(reverse('home:home',))
+            
+                    else:
+                        error = "Wrong username or Password"
+                        return render(request, 'login.html', {'error': error})    
+
                 else:
-                    error = "Wrong username or Password"
-                    return render(request, 'login.html', {'error': error})    
+                        error = "Wrong username or Password"
+                        return render(request, 'login.html', {'error': error})    
                     
                 error = "Wrong username or Password"
                 return render(request, 'login.html', {'error': error})
