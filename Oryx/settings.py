8ec69ca799c0757001a05187fca592df4d1a80d5
Pranshu021 <kingpranshuduneja@gmail.com>
2019-03-04 20:03:45 +0530

    Trying to connect Amazon S3'

--- Oryx/settings.py
+++ Oryx/settings.py
@@ -6,8 +6,9 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 AWS_STORAGE_BUCKET_NAME = 'oryxrating'
-# AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_CUSTOM_DOMAIN = 's3.ap-south-1.amazonaws.com/%s' % AWS_STORAGE_BUCKET_NAME
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_KEY = os.environ.get('AWS_SECRET_KEY')
 
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
