347e460fd2675ea75f9f4d5d3f23aed30d1bea50
Pranshu021 <kingpranshuduneja@gmail.com>
2019-03-05 18:34:59 +0530

    Amazon Bucket Uploading might be working now

--- Oryx/settings.py
+++ Oryx/settings.py
@@ -18,14 +18,14 @@ AWS_S3_OBJECT_PARAMETERS = {
 client = boto3.client(
     's3',
     aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
-    aws_secret_access_key=os.environ.get('AWS_SECRET_KEY'),
+    aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'),
     
 )
 
 # Or via the Session
 session = boto3.Session(
     aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
-    aws_secret_access_key=os.environ.get('AWS_SECRET_KEY'),
+    aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'),
     
 )
 
