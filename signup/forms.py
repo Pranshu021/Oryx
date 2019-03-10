a1e73f88cb86af353c66358f8b8419d8126fa872
Pranshu021 <kingpranshuduneja@gmail.com>
2019-03-11 02:04:20 +0530

    Made some changes

--- signup/forms.py
+++ signup/forms.py
@@ -14,8 +14,11 @@ def Password_validator(password):
     if len(password) < 8:
         raise forms.ValidationError("Password must be atleast of 8 characters")
 
+def ConvertToLowerCase(name):
+        return name.lower()
+
 class UserForm(forms.ModelForm):
-    username = forms.CharField(required=True, max_length=50,widget=forms.TextInput(attrs={'class': 'textfield form-control','placeholder': 'Username'}),validators = [validators.MinLengthValidator(1), Username_check])
+    username = forms.CharField(required=True, max_length=50,widget=forms.TextInput(attrs={'class': 'textfield form-control','placeholder': 'Username'}),validators = [validators.MinLengthValidator(1), Username_check, ConvertToLowerCase])
     email = forms.EmailField(required=True, max_length=50,widget=forms.TextInput(attrs={'class': 'textfield form-control','placeholder': 'Email'}),validators = [validators.MinLengthValidator(1), Email_check])
     password = forms.CharField(required=True, max_length=50,widget=forms.PasswordInput(attrs={'class': 'textfield form-control','placeholder': 'Password'}),validators = [validators.MinLengthValidator(1), Password_validator,])
     confirm_password = forms.CharField(required=True, max_length=50,widget=forms.PasswordInput(attrs={'class': 'textfield form-control','placeholder': 'Re-type Password'}),validators = [validators.MinLengthValidator(1),])
