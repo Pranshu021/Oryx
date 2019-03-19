ad2a2e2121ff9677967c9f6936ea40b36707be75
Pranshu021 <kingpranshuduneja@gmail.com>
2019-03-19 20:31:57 +0530

    Some packages were not imported

--- login/views.py
+++ login/views.py
@@ -7,6 +7,8 @@ from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
 from profiles.models import UserInfo
+import random
+from django.core.mail import send_mail
 
 def LoginView(request):
 
